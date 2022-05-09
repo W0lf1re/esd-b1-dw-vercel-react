4582f90ba4e82d28ab1039ac72ec41ea21064efc
Yvann <yvann.nadal@mail-esd.com>
2022-05-09 11:58:19 +0200

    load data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [users, setUsers] = useState([])
 
   useEffect(() => {
-    fetch(`${process.env.REAC_APP_API_URL}/users`)
+    fetch(`${process.env.REACT_APP_API_URL}/users`)
       .then(response => response.json())
       .then(data => {
         console.log(data);
