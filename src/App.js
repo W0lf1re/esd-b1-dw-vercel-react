e20318c054678f6b9832fc78e9a7731a7dbc1338
Yvann <yvann.nadal@mail-esd.com>
2022-05-09 10:18:38 +0200

    edit app js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello my app</h1>
+        <h1>Hello my app with vercel</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
