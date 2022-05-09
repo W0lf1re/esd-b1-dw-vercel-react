b5f903bf6c1f64d73ca77b075a43a9abacf1e2ae
Yvann <yvann.nadal@mail-esd.com>
2022-05-09 11:56:06 +0200

    load data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(`${process.env.REAC_APP_API_URL}/users`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setUsers(data);
+      })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello my app with vercel</h1>
+        <ul>
+          {users.map(user => (
+            <li>{user.firstName} {user.lastName}</li>
+          ))}
+        </ul>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
